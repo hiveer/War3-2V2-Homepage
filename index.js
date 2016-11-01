de5b58e7cc3ce7acedd9ce56fe17113b311ba9b9
Hiveer <ljp.hiveer@gmail.com>
2016-11-01 16:51:13 +0800

    生成第一轮比赛分组对抗列表

--- index.js
+++ index.js
@@ -123,6 +123,50 @@ $(function(){
     $('#generate_group').attr("disabled", true);
   });
 
+  // 生成第一轮分组列表
+  $("#generate_round_one").click(function() {
+    $.ajax("https://leancloud.cn:443/1.1/classes/group",
+    {
+      headers: { "X-LC-Id": "u13g5ML53PDR4dponmtvMQRu-gzGzoHsz",
+                  "X-LC-Key": "UX3z5HDqeVE911LSHXfzrAqE" },
+      success: function(data){
+        var shuffledList = shuffle(data.results);
+        for(var i = 0 ; i < shuffledList.length; i++) {
+          var result = shuffledList[i];
+          if(i < Math.ceil(shuffledList.length/2)) {
+            $.ajax("https://leancloud.cn:443/1.1/classes/first_round_a",
+            {
+              method: "POST",
+              headers: { "X-LC-Id": "u13g5ML53PDR4dponmtvMQRu-gzGzoHsz",
+                        "X-LC-Key": "UX3z5HDqeVE911LSHXfzrAqE" },
+              contentType: "application/json",
+              processData: false,
+              data: JSON.stringify({
+                left:result.left,
+                right:result.right,
+                status:''} )
+            });
+          }
+          else {
+            $.ajax("https://leancloud.cn:443/1.1/classes/first_round_b",
+            {
+              method: "POST",
+              headers: { "X-LC-Id": "u13g5ML53PDR4dponmtvMQRu-gzGzoHsz",
+                        "X-LC-Key": "UX3z5HDqeVE911LSHXfzrAqE" },
+              contentType: "application/json",
+              processData: false,
+              data: JSON.stringify({
+                left:result.left,
+                right:result.right,
+                status:''} )
+            });
+          }
+        }
+      }
+    });
+    $('#generate_round_one').attr("disabled", true);
+  });
+
   // 获取随机分组列表
   $.ajax("https://leancloud.cn:443/1.1/classes/group",
   {
@@ -139,7 +183,26 @@ $(function(){
         var stats = data.results[i]['status'];
         row$.append($('<td>').html(stats));
         $("#group-list").append(row$);
-        // $("#apply-list").html(row$);
+      }
+    }
+  });
+
+  // 获取第一轮分组A组列表
+  $.ajax("https://leancloud.cn:443/1.1/classes/first_round_a",
+  {
+    headers: { "X-LC-Id": "u13g5ML53PDR4dponmtvMQRu-gzGzoHsz",
+                "X-LC-Key": "UX3z5HDqeVE911LSHXfzrAqE" },
+    success: function(data){
+      $('#group_count').append($('<strong>').html(data.results.length))
+      for(var i = 0 ; i < data.results.length; i++) {
+        var row$ = $('<tr>');
+        var user1 = data.results[i]['left'];
+        row$.append($('<td>').html(user1));
+        var user2 = data.results[i]['right'];
+        row$.append($('<td>').html(user2));
+        var stats = data.results[i]['status'];
+        row$.append($('<td>').html(stats));
+        $("#group-list").append(row$);
       }
     }
   });
