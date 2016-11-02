d68ebdd8a688a83b4ef62f5ebc1229a26a9bf894
Hiveer <ljp.hiveer@gmail.com>
2016-11-02 13:20:12 +0800

    分AB小组

--- index.js
+++ index.js
@@ -193,16 +193,46 @@ $(function(){
     headers: { "X-LC-Id": "u13g5ML53PDR4dponmtvMQRu-gzGzoHsz",
                 "X-LC-Key": "UX3z5HDqeVE911LSHXfzrAqE" },
     success: function(data){
-      $('#group_count').append($('<strong>').html(data.results.length))
-      for(var i = 0 ; i < data.results.length; i++) {
+      for(var i = 0 ; i < data.results.length; i += 2) {
         var row$ = $('<tr>');
-        var user1 = data.results[i]['left'];
-        row$.append($('<td>').html(user1));
-        var user2 = data.results[i]['right'];
-        row$.append($('<td>').html(user2));
-        var stats = data.results[i]['status'];
-        row$.append($('<td>').html(stats));
-        $("#group-list").append(row$);
+        var team1 = data.results[i]['left'] + ', ' + data.results[i]['right'];
+        row$.append($('<td>').html(team1));
+        var team1Status = data.results[i]['status'];
+        row$.append($('<td>').html(team1Status));
+        row$.append($('<td>').html('VS'));
+        var team2 = data.results[i+1]['left'] + ', ' + data.results[i+1]['right'];
+        row$.append($('<td>').html(team2));
+        var team2Status = data.results[i+1]['status'];
+        row$.append($('<td>').html(team2Status));
+        $("#first-round-a").append(row$);
+      }
+    }
+  });
+
+  // 获取第一轮分组B组列表
+  $.ajax("https://leancloud.cn:443/1.1/classes/first_round_b",
+  {
+    headers: { "X-LC-Id": "u13g5ML53PDR4dponmtvMQRu-gzGzoHsz",
+                "X-LC-Key": "UX3z5HDqeVE911LSHXfzrAqE" },
+    success: function(data){
+      for(var i = 0 ; i < data.results.length; i += 2) {
+        var row$ = $('<tr>');
+        var team1 = data.results[i]['left'] + ', ' + data.results[i]['right'];
+        row$.append($('<td>').html(team1));
+        if(i == 10) {var team1Status = "晋级"} else {
+        var team1Status = data.results[i]['status'];
+        };
+        row$.append($('<td>').html(team1Status));
+        row$.append($('<td>').html('VS'));
+        if(i == 10) { var team2 = ""} else {
+        var team2 = data.results[i+1]['left'] + ', ' + data.results[i+1]['right'];
+        };
+        row$.append($('<td>').html(team2));
+        if(i == 10) {var team2Status = ""} else {
+        var team2Status = data.results[i+1]['status'];
+        };
+        row$.append($('<td>').html(team2Status));
+        $("#first-round-b").append(row$);
       }
     }
   });
