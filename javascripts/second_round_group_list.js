5e79ebf826ca1345ddaaa3ff560d676b1dc95a50
Hiveer <ljp.hiveer@gmail.com>
2016-11-24 15:05:43 +0800

    高亮晋级组的颜色

--- javascripts/second_round_group_list.js
+++ javascripts/second_round_group_list.js
@@ -8,8 +8,15 @@ define(function(){
         var row$ = $('<tr>');
         var team1 = data.results[i]['left'] + ', ' + data.results[i]['right'];
         row$.append($('<td>').html(team1));
-        if(i == 10) {var team1Status = "晋级"} else {
-        var team1Status = data.results[i]['status'];
+        // if(i == 10) {var team1Status = "晋级"} else {
+        // var team1Status = data.results[i]['status'];
+        // };
+        if(i == 10) {var team1Status = "<p>晋级</p>"} else {
+          if(data.results[i]['status'] == "晋级"){
+            var team1Status = "<p>" + data.results[i]['status'] + "</p>";
+          } else {
+            var team1Status = data.results[i]['status'];
+          }
         };
         row$.append($('<td>').html(team1Status));
         row$.append($('<td>').html('VS'));
@@ -18,7 +25,12 @@ define(function(){
         };
         row$.append($('<td>').html(team2));
         if(i == 10) {var team2Status = ""} else {
-        var team2Status = data.results[i+1]['status'];
+        // var team2Status = data.results[i+1]['status'];
+          if(data.results[i+1]['status'] == "晋级"){
+            var team2Status = "<p>" + data.results[i+1]['status'] + "</p>";
+          } else {
+            var team2Status = data.results[i+1]['status'];
+          }
         };
         row$.append($('<td>').html(team2Status));
         $("#second-round").append(row$);
