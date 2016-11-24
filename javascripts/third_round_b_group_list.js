2a619df7fed50e697a7233d607aa98434b3c3c80
Hiveer <ljp.hiveer@gmail.com>
2016-11-24 16:29:39 +0800

    获取第三轮的数据

---
+++ javascripts/third_round_b_group_list.js
@@ -0,0 +1,17 @@
+define(function(){
+  $.ajax("https://leancloud.cn:443/1.1/classes/third_round_b",
+  {
+    headers: { "X-LC-Id": "u13g5ML53PDR4dponmtvMQRu-gzGzoHsz",
+                "X-LC-Key": "UX3z5HDqeVE911LSHXfzrAqE" },
+    success: function(data){
+      for(var i = 0 ; i < data.results.length; i++) {
+        var row$ = $('<tr>');
+        var team = data.results[i]['left'] + ', ' + data.results[i]['right'];
+        row$.append($('<td>').html(team));
+        var teamScore = data.results[i]['score'];
+        row$.append($('<td>').html(teamScore));
+        $("#third-round-b").append(row$);
+      }
+    }
+  });
+});
